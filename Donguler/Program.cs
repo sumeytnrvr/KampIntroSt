7bd49629e4c6f1aea8d2fa5feafdc91c2c4bbce2
sumeytnrvr <102673020+sumeytnrvr@users.noreply.github.com>
2023-08-03 01:28:47 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kmapı";
+            string kurs2 = "Programlamaya Giriş Kampı";
+            string kurs3 = "Java";
+
+            //array - dizi
+            //bir çok değişken tutmak yerine kullanılır
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı",
+                "Programlamaya Giriş Kampı",
+                "Java",
+                "Python"};
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For bitti");
+
+            // Dizi temelli yapıları tek tek dönmeye yarar
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);                  
+            }
+            Console.WriteLine("Sayfa Sonu");
+        }
+    }
+}
---
+++ KampIntroSt/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KampIntroSt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety = tip güvenliği
+            string kategoriEtiketi = "Kategori Etiketi";
+            int ogrenciSayisi = 43;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.30;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if(dolarBugun>dolarDun)    
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi");    
+            }
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarlar Butonu");
+            }       
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+            Console.WriteLine(kategoriEtiketi);
+            Console.WriteLine(ogrenciSayisi);  
+            Console.WriteLine(faizOrani);   
+        }
+    }
+}
